ed18a111060895b4daa438b95ab1398a552a2749
Tony Vuolo <34285458+LunarNebula@users.noreply.github.com>
2023-07-01 08:22:17 -0400

    Fixed javadoc formatting in BooleanOperable and BooleanSetOperable

--- src/Algebra/BooleanOperable.java
+++ src/Algebra/BooleanOperable.java
@@ -1,7 +1,7 @@
 package Algebra;
 
 /**
- * A class used to interface with objets that support boolean algebra. These classes
+ * A class used to interface with objects that support boolean algebra. These classes
  * are strictly defined as binary; for example, defining a target member of the class
  * as {@code NOT()} is well-defined (see {@code BooleanSetOperable} for an example of
  * a class where this is not the case).
@@ -10,22 +10,24 @@ package Algebra;
  */
 public interface BooleanOperable<Data extends BooleanOperable<Data>> extends BooleanSetOperable<Data> {
     /**
-     * Finds the logical negation of this BooleanOperable
-     * @return NOT this
+     * Finds the logical {@code NOT} of this {@code BooleanOperable}.
+     * @return {@code NOT this}
      */
     Data NOT();
 
     /**
-     * Finds the logical NOT_AND of this BooleanOperable and another, specified BooleanOperable
-     * @param o the comparator BooleanSetOperable
-     * @return NOT (this AND o) [can also be written (NOT this) OR (NOT o)]
+     * Finds the logical {@code NOT_AND} of this {@code BooleanOperable} and another,
+     * specified {@code BooleanOperable}.
+     * @param o the comparator {@code BooleanSetOperable}.
+     * @return {@code NOT (this AND o)} [can also be written {@code (NOT this) OR (NOT o)}].
      */
     Data NOT_AND(Data o);
 
     /**
-     * Finds the logical NOR of this BooleanOperable and another, specified BooleanOperable
-     * @param o the comparator BooleanSetOperable
-     * @return NOT (this OR o) [can also be written (NOT this) AND (NOT o)]
+     * Finds the logical {@code NOR} of this {@code BooleanOperable} and another,
+     * specified {@code BooleanOperable}.
+     * @param o the comparator {@code BooleanSetOperable}
+     * @return {@code NOT (this OR o)} [can also be written {@code (NOT this) AND (NOT o)}]
      */
     Data NOR(Data o);
 }
--- src/Algebra/BooleanSetOperable.java
+++ src/Algebra/BooleanSetOperable.java
@@ -11,30 +11,34 @@ package Algebra;
  */
 public interface BooleanSetOperable<Data extends BooleanSetOperable<Data>> {
     /**
-     * Finds the logical intersection of this BooleanSetOperable and another, specified BooleanSetOperable
-     * @param o the comparator BooleanSetOperable
-     * @return this AND o
+     * Finds the logical intersection of this {@code BooleanSetOperable} and another,
+     * specified {@code BooleanSetOperable}.
+     * @param o the comparator {@code BooleanSetOperable}.
+     * @return {@code this AND o}
      */
     Data AND(Data o);
 
     /**
-     * Finds the logical union of this BooleanSetOperable and another, specified BooleanSetOperable
-     * @param o the comparator BooleanSetOperable
-     * @return this OR o
+     * Finds the logical union of this {@code BooleanSetOperable} and another,
+     * specified {@code BooleanSetOperable}.
+     * @param o the comparator {@code BooleanSetOperable}.
+     * @return {@code this OR o}
      */
     Data OR(Data o);
 
     /**
-     * Finds the logical disjoint union of this BooleanSetOperable and another, specified BooleanSetOperable
-     * @param o the comparator BooleanSetOperable
-     * @return this XOR o [this OR o AND NOT (this AND o)]
+     * Finds the logical disjoint union ({@code XOR}) of this {@code BooleanSetOperable}
+     * and another, specified {@code BooleanSetOperable}.
+     * @param o the comparator {@code BooleanSetOperable}
+     * @return {@code this XOR o} [{@code this OR o AND NOT (this AND o)}]
      */
     Data XOR(Data o);
 
     /**
-     * Computes the logical AND_NOT for this BooleanSetOperable and another, specified BooleanSetOperable
-     * @param o the comparator BooleanSetOperable
-     * @return this AND NOT o
+     * Computes the logical {@code AND_NOT} for this {@code BooleanSetOperable} and another,
+     * specified {@code BooleanSetOperable}.
+     * @param o the comparator {@code BooleanSetOperable}.
+     * @return {@code this AND NOT o}
      */
     Data AND_NOT(Data o);
 }
