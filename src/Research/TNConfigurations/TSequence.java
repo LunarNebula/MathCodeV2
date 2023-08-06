2e46dbbafab19b04db269c55bfc9c304d4a4e153
Tony Vuolo <34285458+LunarNebula@users.noreply.github.com>
2023-08-05 22:31:12 -0400

    Added T-sequence print capability to TSequence class

--- src/Research/TNConfigurations/TSequence.java
+++ src/Research/TNConfigurations/TSequence.java
@@ -1,6 +1,8 @@
 package Research.TNConfigurations;
 
 public class TSequence {
+    private static final String[] T_SEQUENCE = {"-", "+"};
+
     /**
      * Computes the number of cases covered by one or more of the following formulae:
      * <ul>
@@ -12,9 +14,14 @@ public class TSequence {
      *     <li>Any cyclic permutation of the above.</li>
      * </ul>
      * @param length the length of the T-sequence.
+     * @param print {@code true} if each uncovered T-sequence (independent up to
+     *                          cyclic permutation) should be printed to console.
+     * @param isTSequence {@code true} if the print statement should print a
+     *                                T-sequence, else {@code false} if it should
+     *                                print a binary value.
      * @return the number of solved T-sequences.
      */
-    public static int numberOfCoveredCases(int length) {
+    public static int numberOfCoveredCases(int length, boolean print, boolean isTSequence) {
         final boolean[] tested = new boolean[1 << length];
         final boolean[] track = new boolean[tested.length];
         tested[0] = true;
@@ -46,12 +53,18 @@ public class TSequence {
                     revolveI = rotateBitsRight(revolveI, length);
                     isCovered = (test == 0);
                 }
+                int maxCase = 0;
                 for(int j = 0; j < length; j++) {
                     tested[revolveI] = true;
                     if(isCovered) {
                         track[revolveI] = true;
+                    } else {
+                        maxCase = Math.max(maxCase, revolveI);
                     }
                     revolveI = rotateBitsRight(revolveI, length);
+                }
+                if(print && ! isCovered) {
+                    printBinary(maxCase, length, isTSequence);
                 }
             }
             if(track[i]) {
@@ -78,8 +91,11 @@ public class TSequence {
      *               this value is less than the number of bits required to write the
      *               number, part of the number will be cut off. If it is greater, the
      *               extra bits will be set to zero.
+     * @param isTSequence {@code true} if the value should be printed as a T-sequence,
+     *                                else {@code false} if it should be printed as a
+     *                                raw binary value.
      */
-    public static void printBinary(int num, int length) {
+    public static void printBinary(int num, int length, boolean isTSequence) {
         final StringBuilder builder = new StringBuilder();
         int count = 0;
         if(num == 0) {
@@ -87,11 +103,16 @@ public class TSequence {
             count++;
         }
         while(num != 0 && count++ < length) {
-            builder.insert(0, num & 1);
+            if(isTSequence) {
+                builder.insert(0, T_SEQUENCE[num & 1]);
+            } else {
+                builder.insert(0, num & 1);
+            }
             num >>>= 1;
         }
-        while(count++ < length) {
-            builder.insert(0, 0);
+        if(count < length) {
+            int cropLength = length - count;
+            builder.insert(0, (isTSequence ? T_SEQUENCE[0] : "0").repeat(cropLength));
         }
         System.out.println(builder);
     }
